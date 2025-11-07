7d0cb87883fe7506c7fc9d893c0b2da19b2b11c0
KevX36 <kevincard2007@gmail.com>
2025-11-07 17:01:22 -0400

    music sorted out

--- Assets/SFXUsed.cs
+++ Assets/SFXUsed.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class SFXUsed : MonoBehaviour
 {
+
     // Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">freesound_community</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">Pixabay</a>
     //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">Pixabay</a>
     //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">Pixabay</a>
     //Sound Effect by <a href="https://pixabay.com/users/u_j8f4w9g47f-44040951/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=211710">u_j8f4w9g47f</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=211710">Pixabay</a>
     //
-    //
-    //
+    //Music by <a href="https://pixabay.com/users/the_mountain-3616498/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=133254">Dmitrii Kolesnikov</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=133254">Pixabay</a>
+    //Music by <a href="https://pixabay.com/users/naciux5-50979870/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=368088">Naciux5</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=368088">Pixabay</a>
     //
     //
     //
