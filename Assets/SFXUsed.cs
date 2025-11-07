a3b2a963d1e1c14ea1b4fede0f0851a6c96006fd
KevX36 <kevincard2007@gmail.com>
2025-11-07 10:53:36 -0400

    Initial commit

---
+++ Assets/SFXUsed.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SFXUsed : MonoBehaviour
+{
+    // Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">freesound_community</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">Pixabay</a>
+    //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">Pixabay</a>
+    //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">Pixabay</a>
+    //Sound Effect by <a href="https://pixabay.com/users/u_j8f4w9g47f-44040951/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=211710">u_j8f4w9g47f</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=211710">Pixabay</a>
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+}
