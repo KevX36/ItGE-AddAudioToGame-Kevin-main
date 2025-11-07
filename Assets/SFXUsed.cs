5bad681fa0432a08562d60a49494fb598ff2c984
KevX36 <kevincard2007@gmail.com>
2025-11-07 18:10:29 -0400

    got the damage SFXs

--- Assets/SFXUsed.cs
+++ Assets/SFXUsed.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SFXUsed : MonoBehaviour
 {
-
+    //might be some repeats I had to replace some and wasn't sure which ones I ditched and which ones I kept so at some point I had all of these
     // Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">freesound_community</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">Pixabay</a>
     //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">Pixabay</a>
     //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">Pixabay</a>
@@ -11,8 +11,8 @@ public class SFXUsed : MonoBehaviour
     //Music by <a href="https://pixabay.com/users/the_mountain-3616498/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=133254">Dmitrii Kolesnikov</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=133254">Pixabay</a>
     //Music by <a href="https://pixabay.com/users/naciux5-50979870/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=368088">Naciux5</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=368088">Pixabay</a>
     //
-    //
-    //
+    //Sound Effect by <a href="https://pixabay.com/users/daviddumaisaudio-41768500/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=190270">David Dumais</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=190270">Pixabay</a>
+    //Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=95942">freesound_community</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=95942">Pixabay</a>
     //
     //
     //
